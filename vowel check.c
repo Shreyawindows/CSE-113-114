09a93c8f44113bc38582866ebc71b3de3e623a19
Shreyawindows <shreyadas0429@gmail.com>
2025-08-02 21:51:09 +0600

    Add files via upload

---
+++ vowel check.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    switch(char){
+    case 'a':
+        printf("a is vowel");
+        case 'A':
+            printf("A is vowel");
+        case 'e':
+            printf("e is vowel");
+        case 'E':
+            printf("E is vowel");
+        case 'i':
+            printf("i is vowel");
+        case 'I':
+            printf("I is vowel");
+        case 'o':
+            printf("o is vowel");
+        case 'O':
+            printf("O is vowel");
+        case 'u':
+            printf("u is vowel");
+        case 'U':
+            printf("U is vowel");
+            printf("\n%c is a vowel",char);
+        default:
+            printf("\n%c is not a vowel",char);
+
+    }
+}
